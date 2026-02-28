34bf7d389f9515f280f02de767d473456a4b4e5a
nukkua <marco.salazar.2014.com@gmail.com>
2026-02-27 21:29:54 -0400

    feat: restricted echo

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,7 @@ const std = @import("std");
 const Io  = std.Io;
 
 pub fn main(init: std.process.Init) !void {
+    // init.environ_map.get("Pancakes");
     var reader_buffer: [1024]u8 = undefined;
     var reader = Io.File.stdin().reader(init.io, &reader_buffer);
     const stdin = &reader.interface;
@@ -11,10 +12,10 @@ pub fn main(init: std.process.Init) !void {
     var writer_buffer: [1024]u8 = undefined;
     var writer = Io.File.stdout().writer(init.io, &writer_buffer);
     const stdout = &writer.interface;
-    try get_prompt(init.arena.allocator(), init.io);
 
     while (true) {
-        try stdout.print("> ", .{});
+        const prompt = try get_prompt(init.arena.allocator(), init.io);
+        try stdout.print("{s} $ ", .{prompt});
         try stdout.flush();
 
         const line = stdin.takeDelimiterInclusive('\n') catch |e| switch (e) {
@@ -37,10 +38,16 @@ pub fn main(init: std.process.Init) !void {
                 std.debug.print("zhell: {s}: too many arguments\n", .{command});
             },
             error.PathRequired => std.debug.print("zhell: The Path is Required\n", .{}),
-            error.DirNotFound => {
+            error.NotImplemented => std.debug.print("zhell: Feature not implemented\n", .{}),
+            error.FileNotFound => {
                 const command = args.first();
                 const first_parameter = args.next();
                 std.debug.print("zhell: {s}: {?s}: No such file or directory\n", .{command, first_parameter});
+            },
+            error.NotDir => {
+                const command = args.first();
+                const first_parameter = args.next();
+                std.debug.print("zhell: {s}: {?s}: Not a directory\n", .{command, first_parameter});
             },
             else => unreachable,
         };
@@ -51,6 +58,7 @@ const Commands = enum {
     cd,
     ls,
     echo,
+    clear,
     exit,
     pub fn parse(input: []const u8) !Commands {
         return std.meta.stringToEnum(Commands, input) orelse error.InvalidCommand;
@@ -68,6 +76,7 @@ fn exec_input(line: []u8, io: Io, stdout: anytype) !void {
         .cd => try changeDirectory(&args, io),
         .ls => try list(&args, io),
         .echo => try last_words(&args, stdout),
+        .clear => try clear_screen(&args, stdout),
         .exit => {
             std.debug.print("exit\n", .{});
             std.process.exit(0);
@@ -76,26 +85,18 @@ fn exec_input(line: []u8, io: Io, stdout: anytype) !void {
 }
 
 fn changeDirectory(args: *std.mem.SplitIterator(u8, std.mem.DelimiterType.scalar), io: Io) !void {
-    _ = io;
     const path = args.next() orelse return error.PathRequired;
     const sub_path = std.mem.trimEnd(u8, path, " \n\r\t");
-    if (sub_path.len == 0) return error.PathRequired;
+    if (sub_path.len == 0) return error.NotImplemented;
     if (std.mem.trimEnd(u8, args.rest(), " \n\r\t").len > 0) return error.TooManyArguments;
 
-    var buffer: [std.fs.max_path_bytes + 1]u8 = undefined;
-    @memcpy(buffer[0..sub_path.len], sub_path);
-    buffer[sub_path.len] = 0;
-    const rc = std.os.linux.chdir(buffer[0..sub_path.len:0]);
-
-    if (rc != 0) return error.DirNotFound; // c style errors
+    const dir = try Io.Dir.cwd().openDir(io, sub_path, .{});
+    defer dir.close(io);
+    try std.process.setCurrentDir(io, dir);
 }
-
-
 
 fn list(args: *std.mem.SplitIterator(u8, std.mem.DelimiterType.scalar), io: std.Io) !void {
     if(std.mem.trimEnd(u8, args.rest(), " \n\t\r").len > 0) return error.TooManyArguments; // provisional cuz ls accepts the directory too
-         
-    
     var dir = try Io.Dir.cwd().openDir(io, ".", .{ .iterate = true});
     defer dir.close(io);
 
@@ -105,14 +106,22 @@ fn list(args: *std.mem.SplitIterator(u8, std.mem.DelimiterType.scalar), io: std.
     }
 }
 
-
 fn last_words(args: *std.mem.SplitIterator(u8, std.mem.DelimiterType.scalar), stdout: anytype) !void {
-    const content = std.mem.trimEnd(u8, args.rest(), "\n\t\r");
+    const prevContent = args.next() orelse return error.InvalidCommand;
+    const content = std.mem.trimEnd(u8, prevContent, "\n\t\r");
+
+    if (content.len == 0) return error.NotImplemented;
+    if (args.rest().len > 0) return error.TooManyArguments;
+
     try stdout.print("{s}\n", .{content});
     try stdout.flush();
 }
+fn clear_screen(args: *std.mem.SplitIterator(u8, std.mem.DelimiterType.scalar), stdout: anytype) !void {
+    _ = args;
+    try stdout.print("\x1B[2J\x1B[H", .{});
+}
 
-fn get_prompt(allocator: std.mem.Allocator, io: Io) !void {
-    _ = allocator;
-    _ = io;
+fn get_prompt(allocator: std.mem.Allocator, io: Io) ![]u8 {
+    const cwd: []u8 = try std.process.currentPathAlloc(io, allocator);
+    return cwd;
 }
